be8a0c17c44c3eccb0f90ce325a49324f7bae332
cicero <cicerog.silvestre@gmail.com>
2025-07-10 02:48:02 -0300

    Corrigido contrato com padrao ERC20

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.18;
 
 abstract contract ERC20 {
     event Transfer(address indexed from, address indexed to, uint256 amount);
